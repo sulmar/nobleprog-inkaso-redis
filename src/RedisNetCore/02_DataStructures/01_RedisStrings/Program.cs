9f356e5defd3f0f58cd9cc5255133d9b0ca7a8b4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-03-07 13:50:51 +0100

    Dodanie przykladu Strings

--- src/RedisNetCore/02_DataStructures/01_RedisStrings/Program.cs
+++ src/RedisNetCore/02_DataStructures/01_RedisStrings/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello Redis Strings!");
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello Redis Strings!");
+
+
+// dotnet add package StackExchange.Redis
+
+
+var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions()
+{
+    EndPoints = { "localhost:6379" }
+});
+
+
+var db = muxer.GetDatabase();
+
+var trainerNameKey = new RedisKey("trainer:name");
+
+// SET
+db.StringSet(trainerNameKey, "Marcin");
+
+// GET
+var trainerName = db.StringGet(trainerNameKey);
+
+Console.WriteLine(trainerName);
+
+// INCR
+db.StringIncrement("points", 1);
+db.StringIncrement("points", 1);
+
+// INCRBY
+db.StringIncrement("points", 5);
+
+var points = db.StringGet("points");
+
+if (points.HasValue && points.IsInteger)
+{    
+    Console.WriteLine($"points: {int.Parse(points)}");
+}
+
+var message = db.StringGet("message");
+if (!message.IsNull)
+    Console.WriteLine(message);
+
+
+
+
+
+
