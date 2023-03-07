6e9982db990cb5d9919d4434ce4aa39639f3e062
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-03-07 13:50:34 +0100

    Dodanie przykladu polaczenia

--- src/RedisNetCore/01_Introduction/01_Connect/Program.cs
+++ src/RedisNetCore/01_Introduction/01_Connect/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello Redis!");
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello Redis!");
+
+// dotnet add package StackExchange.Redis
+
+var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions()
+{
+    EndPoints = { "localhost:6379" }
+});
+
+
+var db = muxer.GetDatabase();
+
+// PING
+var response = await db.PingAsync();
+
+Console.WriteLine($"The ping took {response.TotalMilliseconds} ms");
+
+
--- src/RedisNetCore/01_Introduction/02_Pipelining/Program.cs
+++ src/RedisNetCore/01_Introduction/02_Pipelining/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello Redis Pipelining!");
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello Redis Pipelining!");
+
+
+// dotnet add package StackExchange.Redis
+
+
+var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions()
+{
+    EndPoints = { "localhost:6379" }
+});
+
+
+var db = muxer.GetDatabase();
+
+// PING
+
+var batch = db.CreateBatch();
+
+// gwarantuje kolejnosc wykonywania polecen
+
+for (int i = 0; i < 1000; i++)
+{
+    var response = await batch.PingAsync();
+    Console.WriteLine($"The ping took {response.TotalMilliseconds} ms");
+}
+
+batch.Execute();
+
+
